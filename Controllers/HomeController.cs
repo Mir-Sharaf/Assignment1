d0aff05580e5579a4b58bccaf5ff8506321c829e
Mir-Sharaf <mirsharaf.ca@gmail.com>
2023-12-16 18:27:01 -0500

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using Assignment1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Assignment1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //GET /home/RsvpForm
+        public IActionResult RsvpForm()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Submit(CompetitionInvite invite)
+        {
+            // Check which button was pressed
+            var button = Request.Form["submitButton"].ToString();
+
+            switch (button)
+            {
+                case "Accept Invitation":
+                    invite.WillAttend = true;
+                    break;
+                case "Send Regrets":
+                    invite.WillAttend = false;
+                    break;
+            }
+
+            return RedirectToAction("Thanks", invite);
+        }
+
+        public ActionResult Thanks(CompetitionInvite invite)
+        {
+            return View(invite);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/CompetitionInvite.cs
@@ -0,0 +1,25 @@
+﻿namespace Assignment1.Models
+{
+     // Define the enumeration
+    public enum TechnicalInterest
+    {
+        IoT,
+        Cognitive,
+        Wearable,
+        AR_VR
+
+    }
+
+    public class CompetitionInvite
+    {
+        public string Name { get; set; } = "";
+        public string Email { get; set; }= "";
+        public string PhoneNumber { get; set; } = "";
+        public bool WillAttend { get; set; } = false;
+
+        // New properties
+        public string Address { get; set; } = "";
+        public string TwitterAccount { get; set; } = "";  
+        public TechnicalInterest Interest { get; set; }     
+    }
+}
